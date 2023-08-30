7a1f477680174bc0efd5c17cdcecc72cbd1356cf
Syedmasood-git <masoodhussaini46@gmail.com>
2023-08-30 17:29:01 +0530

    changed full js and html code to fetch data

--- script.js
+++ script.js
@@ -1,39 +1,75 @@
+const url="https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json";
+const all_cards=document.getElementById('all-cards')
+async function fetchData() {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      getMenu(result)
+    } catch (error) {
+      console.log('error fetching');
+    }
+  }
 // getMenu function
-function getMenu() {
-    return fetch('https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Error fetching menu:', error);
-      });
+function getMenu(data) {
+    console.log('Menu :',data);
+    //          <div class="cards mb-4  mx-auto">
+    //             <div class="img mx-auto d-flex align-items-center">
+    //               <img class="w-100" src="https://source.unsplash.com/random/1920x1080/?pizza" alt="Pizza" />
+    //             </div>
+    //             <div class="content d-flex justify-content-between">
+    //               <p>Pizza</p>
+    //               <img src="./Assets/plus (1) 1.svg" />
+    //             </div>
+    //             <p class="price py-2">$8.99/-</p>
+    //           </div>
+    //           <div class="cards mb-4 mx-auto">
+    //             <div class="img mx-auto d-flex align-items-center">
+    //               <img class="w-100" src="https://source.unsplash.com/random/1920x1080/?tacos" alt="burger" />
+    //             </div>
+    //             <div class="content d-flex justify-content-between">
+    //               <p>Tacos</p>
+    //               <img src="./Assets/plus (1) 1.svg" />
+    //             </div>
+    //             <p class="price py-2">$3.99/-</p>
+    //           </div>
+  data.forEach((card)=>{
+    const cardContainer=document.createElement('div');
+    // let names=card.name.toLowerCase();
+    cardContainer.className='cards mb-4  mx-auto';
+    cardContainer.innerHTML=`<div class="img mx-auto d-flex align-items-center">
+                             <img class="w-100" src="${card.imgSrc}" alt="${card.name}" />
+                         </div>
+                         <div class="content d-flex justify-content-between">
+                           <p>${card.name}</p>
+                           <img src="./Assets/plus (1) 1.svg" />
+                         </div>
+                         <p class="price py-2">${card.price}</p> `
+    all_cards.appendChild(cardContainer);
+
+  })
   }
   
- // TakeOrder function
-function takeOrder() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const menu = ['Cheeseburger', 'Pizza', 'Tacos', 'Sushi', 'Pasta'];
-        const burgers = [];
-        
-        // Select 3 random burgers from the menu
-        while (burgers.length < 3) {
-          const randomIndex = Math.floor(Math.random() * menu.length);
-          const randomBurger = menu[randomIndex];
-          if (!burgers.includes(randomBurger)) {
-            burgers.push(randomBurger);
-          }
+
+async function takeOrder(){
+  try{
+    const response=await fetch(url);
+    const data=await response.json();
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        const orders=[];
+        for(let i=0;i<3;i++){
+          const randomIndex=Math.floor(Math.random()*data.length);
+          orders.push(data[randomIndex].name);
         }
-  
-        const order = {
-          burgers: burgers
-        };
-  
-        resolve(order);
-      }, 2500);
-    });
+        resolve(orders);
+        console.log('Orders ',orders);
+      },2000)
+    })
   }
+  catch(error){
+    console.log('error in take order:',error);
+  }
+}
   
   
   // orderPrep function
@@ -44,12 +80,11 @@ function takeOrder() {
           order_status: true,
           paid: false
         };
-        if(orderStatus.order_status==true){
+        if(orderStatus.order_status==true && orderStatus.paid==false){
             resolve(orderStatus);
-        }
-        else{
+            console.log('order prep:',orderStatus);
+        }else
             reject(orderStatus);
-        }
       }, 1500);
     });
   }
@@ -62,11 +97,12 @@ function takeOrder() {
           order_status: true,
           paid: true
         };
-        if(orderStatus.paid===true){
+        if(orderStatus.order_status===true && orderStatus.paid==true){
             resolve(orderStatus);
+            console.log('payment:',orderStatus);
         }
         else{
-            reject(orderStatus);
+            reject('Payment failed',orderStatus);
         }
       }, 1000);
     });
@@ -77,32 +113,20 @@ function takeOrder() {
     alert('Thank you for eating with us today!');
   }
   
-  // Promise chaining
-  getMenu()
-    .then(() => {
-      return takeOrder();
-    })
-    .then(order => {
-      console.log('Order:', order);
-      return orderPrep();
-    })
-    .then(orderStatus => {
-      console.log('Order Preparation:', orderStatus);
-      return payOrder();
-    })
-    .catch(orderStatus =>{
-        console.log('Order Prerparation:',orderStatus);
-        console.log("Preparation Unsucessfull");
-    })
-    .then(orderStatus => {
-      console.log('Order Paid:', orderStatus);
+
+  async function restaurantProcess(){
+    try{
+      await fetchData()
+      await takeOrder();
+      await orderPrep();
+      const payorder = await payOrder();
+      if(payorder.paid){
         thankyouFnc();
-    })
-    .catch(orderStatus=>{
-        console.log('Order Paid:',orderStatus);
-        console.log("Payment Unsucessfull");
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      }
+    }
+    catch(error){
+      console.log('error',error);
+    }
+  }
+  restaurantProcess();
   
